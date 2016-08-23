6e90d7515d1c0d757b3ab293f38d5a86a4bbf782
Daniel Melton <daniel.melton@capitalone.com>
2016-08-23 13:26:55 -0400

    changed phone type to accept String

--- src/main/java/com/springapp/mvc/model/User.java
+++ src/main/java/com/springapp/mvc/model/User.java
@@ -17,7 +17,7 @@ public class User {
 
     private String username;
     private String email;
-    private int phone;
+    private String phone;
 
     public int getId() {
         return id;
@@ -43,11 +43,11 @@ public class User {
         this.email = email;
     }
 
-    public int getPhone() {
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(int phone) {
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
